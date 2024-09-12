0aefdd5d83cda9d30969f9cc05ad77e89f2d872a
Simon <Simon@LAPTOP-GFMLGC10>
2024-09-12 10:52:03 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿namespace ChessBoard
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Pick a number between 1-10: ");
+            int number = Convert.ToInt32(Console.ReadLine());
+
+            Console.OutputEncoding = System.Text.Encoding.Unicode;
+
+            for (int i = 0; i < number; i++)
+            {
+                for (int j = 0; j < number; j++)
+                {
+                    int v = j % 2;
+                    int w = i % 2;
+                    string row = v == w ? "◼︎" : "◻︎";
+                    Console.Write(row);
+                }
+                Console.WriteLine("");
+            }
+        }
+    }
+}
